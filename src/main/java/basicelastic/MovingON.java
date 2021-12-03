8bea923bba4a487b1074e8ad9c71881da87c4ba8
Alon <a12345e@gmail.com>
2021-12-03 02:31:30 +0200

    works

--- src/main/java/basicelastic/MovingON.java
+++ src/main/java/basicelastic/MovingON.java
@@ -1,24 +1,34 @@
 package basicelastic;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.spark.api.java.function.FlatMapFunction;
 import org.apache.spark.api.java.function.MapFunction;
 import org.apache.spark.sql.*;
 import org.elasticsearch.spark.rdd.api.java.JavaEsSpark;
 import scala.collection.JavaConverters;
 
 import javax.xml.bind.DatatypeConverter;
+import java.io.IOException;
 import java.io.Serializable;
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.*;
-import java.util.stream.Collectors;
-
 
 
 public class MovingON implements Serializable {
+    public List<String> asIfItIsHamster(String tableRowInJsonFormat) throws IOException, NoSuchAlgorithmException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        Map<String,Object> result =
+                new ObjectMapper().readValue(tableRowInJsonFormat, Map.class);
+        String serialziedAsJsonString = objectMapper.writeValueAsString(result);
+       MessageDigest md5 = MessageDigest.getInstance("MD5");
+       String id = DatatypeConverter.printHexBinary(md5.digest(serialziedAsJsonString.getBytes(StandardCharsets.UTF_8))).toLowerCase(Locale.ROOT);
+       result.put("id",id);
+       return Arrays.asList(objectMapper.writeValueAsString(result));
+    }
 
-    public String rowJsonStr(Row row){
+    public String prepareJsonToHamster(Row row){
         scala.collection.Map<String,String> map = row. getValuesMap(JavaConverters.asScalaIteratorConverter(Arrays.asList(row.schema().fieldNames()).iterator()).asScala().toSeq());
         Map<String,String> javaMap = new HashMap<>();
         scala.collection.Set keys =  map.keySet();
@@ -33,10 +43,6 @@ public class MovingON implements Serializable {
 
         try {
             ObjectMapper objectMapper = new ObjectMapper();
-            String serialziedAsJsonString = objectMapper.writeValueAsString(javaMap);
-            MessageDigest md5 = MessageDigest.getInstance("MD5");
-            String id = DatatypeConverter.printHexBinary(md5.digest(serialziedAsJsonString.getBytes(StandardCharsets.UTF_8))).toLowerCase(Locale.ROOT);
-            javaMap.put("id",id);
             return  objectMapper.writeValueAsString(javaMap);
         }catch(Exception ex){
             ex.printStackTrace();
@@ -52,12 +58,13 @@ public class MovingON implements Serializable {
         SQLContext sql = new SQLContext(spark);
         Dataset<Row> dset = sql.sql("select * from somedb.sometable");
         dset.show();
-        String forTest = rowJsonStr(dset.first());
-        System.out.println("------------------------   forTest ---------------------------------" + forTest);
-        Dataset<String> dsetStrings = dset.map((MapFunction<Row, String>) row -> {
-            return rowJsonStr(row);
+        Dataset<String> strings4Hamster = dset.map((MapFunction<Row, String>) row -> {
+            return prepareJsonToHamster(row);
         }, Encoders.STRING());
-        dsetStrings.show();
+        strings4Hamster.show();
+        System.out.println("This is to be sent to Hamster");
+        Dataset<String> hamsterResults = strings4Hamster.flatMap((FlatMapFunction<String, String>)
+               x ->  { return asIfItIsHamster(x).iterator() ;} ,Encoders.STRING());
         Map<String,String> map = new HashMap<String,String>();
         map.put("es.nodes","localhost:9200");
         map.put("es.nodes.discovery","false");
@@ -65,11 +72,7 @@ public class MovingON implements Serializable {
         map.put("es.write.operation","upsert");
         map.put("es.mapping.id","id");
         map.put("es.mapping.version.type","external");
-
-
-
-
-        JavaEsSpark.saveJsonToEs(dsetStrings.toJavaRDD(),"alon-first/doc",map);
+        JavaEsSpark.saveJsonToEs(hamsterResults.toJavaRDD(),"alon-{type}/doc",map);
     }
     public final static void main(String args[]) throws NoSuchAlgorithmException {
         System.out.println("Alon");
